cd8fd7bbd174a7dd573bc1b047b048f99635e570
heecheul <heecheul1@gmail.com>
2020-02-07 15:55:44 +0900

    modified:   metas/11_1.js

--- metas/11_1.js
+++ metas/11_1.js
@@ -96,6 +96,268 @@ var hymn_info={
 			sfn : "no"
 			}
 		]
+	},
+	{
+		type : "half",
+		gclef_sounds : [
+			{ mrk : "1E", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1B", 
+			stem : "up",
+			sfn : "no"}
+		],
+		fclef_sounds : [
+			{ mrk : "1G",
+			stem : "down",
+			sfn : "no"},
+			{ mrk : "1E", 
+			stem : "down",
+			sfn : "no"}
+		],
+		captions : [
+			"믿",
+			"하",
+			"전",
+			"고"
+		]
+	},
+	{
+		type : "quarterdot",
+		gclef_sounds : [
+			{ mrk : "1E", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1C", 
+			stem : "up",
+			sfn : "no"}
+		],
+		fclef_sounds : [
+			{ mrk : "1G",
+			stem : "down",
+			sfn : "no"},
+			{ mrk : "1C", 
+			stem : "down",
+			sfn : "no"}
+		],
+		captions : [
+			"음",
+			"나",
+			"쟁",
+			"난"
+		]
+	},
+	{
+		type : "eighth",
+		gclef_sounds : [
+			{ mrk : "1F", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1D", 
+			stem : "up",
+			sfn : "no"}
+		],
+		fclef_sounds : [
+			{ mrk : "2B",
+			stem : "down",
+			sfn : "no"},
+			{ mrk : "1B", 
+			stem : "down",
+			sfn : "no"}
+		],
+		captions : [
+			"의",
+			"님",
+			"과",
+			"길"
+		]
+	},
+	{
+		type : "halfdot",
+		gclef_sounds : [
+			{ mrk : "1G", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1E", 
+			stem : "up",
+			sfn : "no"}
+		],
+		fclef_sounds : [
+			{ mrk : "2B",
+			stem : "down",
+			sfn : "no"},
+			{ mrk : "1E", 
+			stem : "down",
+			sfn : "no"}
+		],
+		captions : [
+			"조",
+			"사",
+			"환",
+			"걷"
+		]
+	},
+	{
+		type : "quarter",
+		gclef_sounds : [
+			{ mrk : "2A", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1E", 
+			stem : "up",
+			sfn : "no"}
+		],
+		fclef_sounds : [
+			{ mrk : "2C",
+			stem : "down",
+			sfn : "no"},
+			{ mrk : "1A", 
+			stem : "down",
+			sfn : "no"}
+		],
+		captions : [
+			"상",
+			"랑",
+			"난",
+			"는"
+		]
+	},
+	{
+		type : "quarter",
+		gclef_sounds : [
+			{ mrk : "1G", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1E", 
+			stem : "up",
+			sfn : "no"}
+		],
+		fclef_sounds : [
+			{ mrk : "2B",
+			stem : "down",
+			sfn : "no"},
+			{ mrk : "1B", 
+			stem : "down",
+			sfn : "no"}
+		],
+		captions : [
+			"들",
+			"나",
+			"주",
+			"백"
+		]
+	},
+	{
+		type : "quarter",
+		gclef_sounds : [
+			{ mrk : "1E", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1B", 
+			stem : "up",
+			sfn : "no"}
+		],
+		fclef_sounds : [
+			{ mrk : "1G",
+			stem : "down",
+			sfn : "no"},
+			{ mrk : "1B", 
+			stem : "down",
+			sfn : "no"}
+		],
+		captions : [
+			"의",
+			"를",
+			"님",
+			"성"
+		]
+	},
+	{
+		type : "quarter",
+		gclef_sounds : [
+			{ mrk : "1E", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1B", 
+			stem : "up",
+			sfn : "no"}
+		],
+		fclef_sounds : [
+			{ mrk : "1F",
+			stem : "down",
+			sfn : "no"},
+			{ mrk : "1B", 
+			stem : "down",
+			sfn : "no"}
+		],
+		captions : [
+			"하",
+			"인",
+			"막",
+			"힘"
+		]
+	},
+	{
+		type : "quarter",
+		gclef_sounds : [
+			{ mrk : "1D", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1B", 
+			stem : "up",
+			sfn : "no"}
+		],
+		fclef_sounds : [
+			{ mrk : "2A",
+			stem : "down",
+			sfn : "no"},
+			{ mrk : "1B", 
+			stem : "down",
+			sfn : "no"}
+		],
+		captions : [
+			"나",
+			"도",
+			"으",
+			"주"
+		]
+	},
+	{
+		type : "whole",
+		gclef_sounds : [
+			{ mrk : "1E", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1B", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "2A/2B/2B/2B/2B/2B",
+			types : "quarterrest/triplet/triplet/triplet/quarter/quarter",
+			stem : "no/up/up/up/up/up",
+			sfn : "no/no/no/no/no/no"},
+			{ mrk : "2A/1G/1G/1G/1G/1G",
+			types : "quarterrest/triplet/triplet/triplet/quarter/quarter",
+			stem : "no/up/up/up/up/up",
+			sfn : "no/no/no/no/no/no"},
+			{ mrk : "2A/1E/1E/1E/1E/1E",
+			types : "quarterrest/triplet/triplet/triplet/quarter/quarter",
+			stem : "no/up/up/up/up/up",
+			sfn : "no/no/no/no/no/no"}
+		],
+		fclef_sounds : [
+			{ mrk : "1G",
+			stem : "down",
+			sfn : "no"},
+			{ mrk : "1E", 
+			stem : "down",
+			sfn : "no"}
+		],
+		captions : [
+			"님",
+			"해",
+			"사",
+			"고"
+		]
 	}
 	]
 };
