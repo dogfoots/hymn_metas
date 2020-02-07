3e6b3023520650756aa7c10f7e2550da1d497ced
heecheul <heecheul1@gmail.com>
2020-02-07 15:34:11 +0900

    new file:   metas/11_1.js

---
+++ metas/11_1.js
@@ -0,0 +1,77 @@
+var hymn_info={
+
+	no : 11,
+	title : "믿음의 조상들의 하나님",
+	en_title : "God of Our Fathers",
+	bbverse : "신 9:29",
+	theme : "경배와 찬양",
+	writer_sbt : {
+		name : "Daniel C. Roberts",
+		desc : "1876(1841-1907)"
+	},
+
+	writer_mdy : {
+		name : "George W. Warren",
+		desc : "1888(1828-1902)"
+	},
+
+	transp : {
+		type : "flat",
+		cnt : 3
+	},
+
+	meterson : 4,
+	metermother : 4,
+	meteroffset : 0,
+	versecnt : 4,
+	musictempo : 2.6,
+
+	notes : [
+	{
+		type : "half",
+		gclef_sounds : [
+			{ mrk : "2B", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1G", 
+			stem : "up",
+			sfn : "no"},
+			{ mrk : "1E", 
+			stem : "up",
+			sfn : "no"}
+		]
+	},
+	{
+		type : "quarterrest",
+		gclef_sounds : [
+			{ mrk : "2A", 
+			stem : "up",
+			sfn : "no"}
+		]
+	},
+	{
+		type : "quarter",
+		gclef_sounds : [
+			{ mrk : "2B/2B/2B", 
+			types : "triplet/triplet/triplet",
+			stem : "up/up/up",
+			sfn : "no/no/no"},
+			{ mrk : "1G/1G/1G", 
+			types : "triplet/triplet/triplet",
+			stem : "up/up/up",
+			sfn : "no/no/no"},
+			{ mrk : "1E/1E/1E", 
+			types : "triplet/triplet/triplet",
+			stem : "up/up/up",
+			sfn : "no/no/no"}
+		],
+		fclef_sounds : [
+			{ mrk : "1C",
+			types : "wholerest",
+			stem : "up",
+			sfn : "no"
+			}
+		]
+	}
+	]
+};
